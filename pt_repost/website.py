c357e628cf480e4e4d116eaa466b70f01c2715ab
Trim21 <trim21.me@gmail.com>
2024-11-23 13:32:05 +0800

    fix: pack

--- pt_repost/website.py
+++ pt_repost/website.py
@@ -37,6 +37,9 @@ class SSD(Website):
 
         guess: dict[str, str] = guessit.guessit(torrent_name)
 
+        if "season" in guess and "episode" not in guess:
+            options["pack"] = "1"
+
         match guess.get("type"):
             case "movie":
                 options["type"] = "501"
